319c3e02dcae8038c40f24aaefc7cd62a9ba59b8
Aaron Klump <sourcecode@intheloftstudios.com>
2017-05-18 10:46:12 -0700

    update build

--- .web_package/hooks/build/3_dist.sh
+++ .web_package/hooks/build/3_dist.sh
@@ -3,7 +3,13 @@
 # @file
 # Copy distribution files to /dist
 # 
-sleep 3
+# Allow time for all CodeKit to minify.
+while [ ! -f "$7/LoftToggler.min.js" ]; do
+  sleep 1
+done
+
+test -h "$7/dist" && rm "$7/dist"
 test -d "$7/dist" || mkdir -p "$7/dist"
+
 cp "$7/LoftToggler.js" "$7/dist/"
 cp "$7/LoftToggler.min.js" "$7/dist/"
